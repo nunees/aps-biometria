e896ceb7bb4efbe9603be0e3d627f7328b04e169
nunees <felipe_silva_nunes@hotmail.com>
2022-11-15 01:23:45 -0300

    feat: add initial configuration file

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,10 +1,15 @@
 package app;
 
-import db.SQLiteJDBCDriverConnection;
+import db.DatabaseInitTables;
 import dto.UsuarioDto;
 import janelas.JanelaLogin;
 import janelas.JanelaPrincipal;
+import utils.Log;
+import utils.TipoLog;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -12,10 +17,26 @@ public class Application {
     protected JanelaPrincipal janelaPrincipal;
     public static UsuarioDto usuario;
 
-    public Application() throws IOException {
+    private static File InitialSetupFile;
+    public Application() throws IOException, SQLException {
+        DatabaseInitTables databaseInitTables = new DatabaseInitTables();
+        Log.print(TipoLog.INFO, "Procurando arquivo de configuracao");
+        InitialSetupFile = new File("./app.dat");
+        if(!InitialSetupFile.exists() && !InitialSetupFile.isDirectory()){
+            Log.print(TipoLog.ALERTA, "File not exists!");
+            boolean fileCreated = InitialSetupFile.createNewFile();
+            if(fileCreated){
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(InitialSetupFile, true));
+                bufferedWriter.append("1");
+                bufferedWriter.close();
+                Log.print(TipoLog.INFO, "File Created");
+                Log.print(TipoLog.INFO, "Criando banco de dados");
+                databaseInitTables.createTables();
+            }
+        }
 
         usuario = new UsuarioDto();
-        janelaPrincipal = new JanelaPrincipal(usuario);
+        janelaPrincipal = new JanelaPrincipal();
         janelaPrincipal.setVisible(true);
         JanelaLogin janelaLogin = new JanelaLogin(janelaPrincipal);
         janelaLogin.getJDialog().setVisible(true);
@@ -26,7 +47,7 @@ public class Application {
     }
 
     public static void main(String[] args) throws SQLException, IOException {
-        new SQLiteJDBCDriverConnection();
+        Log.print(TipoLog.INFO, "Aplicacao iniciada");
         new Application();
     }
 
